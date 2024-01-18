92b0ed0b0b8207ff37fb523ac18af6a3f87da4c9
Zainab-Dahshan <zainabeldahshan@gmail.com>
2024-01-18 02:52:04 -0800

    nop_opcode.c

---
+++ nop_opcode.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+  *f_nop- a function that does not do anything
+  *@head: The stack head
+  *@counter: line_number
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+(void) counter;
+(void) head;
+}
